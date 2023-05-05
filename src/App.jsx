30e7034d63189338bb491331f6e727b6f0e758af
GluteusMedius <zarnauskas97@gmail.com>
2023-05-05 13:45:07 +0300

    tic-tack game init

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './styles/reset.css';
+import './styles/App.css';
+import Square from './components/ui/Square';
+
+function App() {
+  return (
+    <div className="container">
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+    </div>
+  );
+}
+export default App;
