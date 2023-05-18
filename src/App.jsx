a5d9c837c13e72add8b5d1b3f1dee9f3a8f79e30
GluteusMedius <zarnauskas97@gmail.com>
2023-05-18 23:48:13 +0300

    set winner

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,170 @@ import { useState } from 'react';
 
 function App() {
   const [clickVal, setClickVal] = useState(0);
-  const [stepInfo, setStepInfo] = useState({});
+  const [stepInfo, setStepInfo] = useState([]);
   function counter() {
     setClickVal(clickVal + 1);
   }
   function setWinner(id, value) {
-    setStepInfo((id, value) + stepInfo);
-    console.log(stepInfo);
+    const newStepInfo = [...stepInfo];
+    newStepInfo.push({ identification: id, sign: value });
+    setStepInfo(newStepInfo);
+
+    if (
+      (newStepInfo.filter(
+        (step) => step.identification === 1 && step.sign === 'X',
+      ).length > 0 &&
+        newStepInfo.filter(
+          (step) => step.identification === 2 && step.sign === 'X',
+        ).length > 0 &&
+        newStepInfo.filter(
+          (step) => step.identification === 3 && step.sign === 'X',
+        ).length > 0) ||
+      (newStepInfo.filter(
+        (step) => step.identification === 4 && step.sign === 'X',
+      ).length > 0 &&
+        newStepInfo.filter(
+          (step) => step.identification === 5 && step.sign === 'X',
+        ).length > 0 &&
+        newStepInfo.filter(
+          (step) => step.identification === 6 && step.sign === 'X',
+        ).length > 0) ||
+      (newStepInfo.filter(
+        (step) => step.identification === 7 && step.sign === 'X',
+      ).length > 0 &&
+        newStepInfo.filter(
+          (step) => step.identification === 8 && step.sign === 'X',
+        ).length > 0 &&
+        newStepInfo.filter(
+          (step) => step.identification === 9 && step.sign === 'X',
+        ).length > 0) ||
+      (newStepInfo.filter(
+        (step) => step.identification === 1 && step.sign === 'X',
+      ).length > 0 &&
+        newStepInfo.filter(
+          (step) => step.identification === 4 && step.sign === 'X',
+        ).length > 0 &&
+        newStepInfo.filter(
+          (step) => step.identification === 7 && step.sign === 'X',
+        ).length > 0) ||
+      (newStepInfo.filter(
+        (step) => step.identification === 2 && step.sign === 'X',
+      ).length > 0 &&
+        newStepInfo.filter(
+          (step) => step.identification === 5 && step.sign === 'X',
+        ).length > 0 &&
+        newStepInfo.filter(
+          (step) => step.identification === 8 && step.sign === 'X',
+        ).length > 0) ||
+      (newStepInfo.filter(
+        (step) => step.identification === 3 && step.sign === 'X',
+      ).length > 0 &&
+        newStepInfo.filter(
+          (step) => step.identification === 6 && step.sign === 'X',
+        ).length > 0 &&
+        newStepInfo.filter(
+          (step) => step.identification === 9 && step.sign === 'X',
+        ).length > 0) ||
+      (newStepInfo.filter(
+        (step) => step.identification === 1 && step.sign === 'X',
+      ).length > 0 &&
+        newStepInfo.filter(
+          (step) => step.identification === 5 && step.sign === 'X',
+        ).length > 0 &&
+        newStepInfo.filter(
+          (step) => step.identification === 9 && step.sign === 'X',
+        ).length > 0) ||
+      (newStepInfo.filter(
+        (step) => step.identification === 3 && step.sign === 'X',
+      ).length > 0 &&
+        newStepInfo.filter(
+          (step) => step.identification === 5 && step.sign === 'X',
+        ).length > 0 &&
+        newStepInfo.filter(
+          (step) => step.identification === 7 && step.sign === 'X',
+        ).length > 0)
+    ) {
+      console.log('X Win');
+    } else if (
+      (newStepInfo.filter(
+        (step) => step.identification === 1 && step.sign === 'O',
+      ).length > 0 &&
+        newStepInfo.filter(
+          (step) => step.identification === 2 && step.sign === 'O',
+        ).length > 0 &&
+        newStepInfo.filter(
+          (step) => step.identification === 3 && step.sign === 'O',
+        ).length > 0) ||
+      (newStepInfo.filter(
+        (step) => step.identification === 4 && step.sign === 'O',
+      ).length > 0 &&
+        newStepInfo.filter(
+          (step) => step.identification === 5 && step.sign === 'O',
+        ).length > 0 &&
+        newStepInfo.filter(
+          (step) => step.identification === 6 && step.sign === 'O',
+        ).length > 0) ||
+      (newStepInfo.filter(
+        (step) => step.identification === 7 && step.sign === 'O',
+      ).length > 0 &&
+        newStepInfo.filter(
+          (step) => step.identification === 8 && step.sign === 'O',
+        ).length > 0 &&
+        newStepInfo.filter(
+          (step) => step.identification === 9 && step.sign === 'O',
+        ).length > 0) ||
+      (newStepInfo.filter(
+        (step) => step.identification === 1 && step.sign === 'O',
+      ).length > 0 &&
+        newStepInfo.filter(
+          (step) => step.identification === 4 && step.sign === 'O',
+        ).length > 0 &&
+        newStepInfo.filter(
+          (step) => step.identification === 7 && step.sign === 'O',
+        ).length > 0) ||
+      (newStepInfo.filter(
+        (step) => step.identification === 2 && step.sign === 'O',
+      ).length > 0 &&
+        newStepInfo.filter(
+          (step) => step.identification === 5 && step.sign === 'O',
+        ).length > 0 &&
+        newStepInfo.filter(
+          (step) => step.identification === 8 && step.sign === 'O',
+        ).length > 0) ||
+      (newStepInfo.filter(
+        (step) => step.identification === 3 && step.sign === 'O',
+      ).length > 0 &&
+        newStepInfo.filter(
+          (step) => step.identification === 6 && step.sign === 'O',
+        ).length > 0 &&
+        newStepInfo.filter(
+          (step) => step.identification === 9 && step.sign === 'O',
+        ).length > 0) ||
+      (newStepInfo.filter(
+        (step) => step.identification === 1 && step.sign === 'O',
+      ).length > 0 &&
+        newStepInfo.filter(
+          (step) => step.identification === 5 && step.sign === 'O',
+        ).length > 0 &&
+        newStepInfo.filter(
+          (step) => step.identification === 9 && step.sign === 'O',
+        ).length > 0) ||
+      (newStepInfo.filter(
+        (step) => step.identification === 3 && step.sign === 'O',
+      ).length > 0 &&
+        newStepInfo.filter(
+          (step) => step.identification === 5 && step.sign === 'O',
+        ).length > 0 &&
+        newStepInfo.filter(
+          (step) => step.identification === 7 && step.sign === 'O',
+        ).length > 0)
+    ) {
+      console.log('O Win');
+    }
   }
+
+  // console.log(stepInfo);
+
   return (
     <div className="container">
       <Square
